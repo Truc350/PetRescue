629a0f5eb098c80d0c339f73c0c0540f344d55e7
Truc350 <23130350@st.hcmuaf.edu.vn>
2025-11-30 11:21:13 +0700

    tạo trang quên mật khẩu, đăng nhập, đăng ký

--- petHealth/my_app/views.py
+++ petHealth/my_app/views.py
@@ -7,3 +7,13 @@ def get_home(request):
     return render(request, 'home.html')  # tra ve trang my_app trong trang web
 def getChatWithAI(request):
     return render(request, 'fontend/chatWithAI.html')
+def getFooter(request):
+    return render(request, 'fontend/footer.html')
+def getHeader(request):
+    return render(request, 'fontend/header.html')
+def getLogin(request):
+    return render(request, 'fontend/login.html')
+def getRegister(request):
+    return render(request, 'fontend/register.html')
+def getForgotPassword(request):
+    return render(request, 'fontend/forgot-password.html')
