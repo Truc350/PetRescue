6d9d00e425eb89c8aa018b65248a981b1c99a46c
tram <23130341@st.hcmuaf.edu.vn>
2025-11-30 12:55:43 +0700

    trang chủ

--- petHealth/my_app/urls.py
+++ petHealth/my_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 # from my_app.views import get_home
 urlpatterns = [
     path('home', views.get_home),
-        path('ChatWithAI', views.getChatWithAI),
+    path('ChatWithAI', views.getChatWithAI),
+    path('footer', views.getFooter),
+    path('header', views.getHeader),
+    path('homePage', views.getHomePage),
+
 ]
