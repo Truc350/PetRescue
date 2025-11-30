5213939d35c46e3c240045fc98b76d35a6975575
Truc350 <23130350@st.hcmuaf.edu.vn>
2025-11-30 15:14:10 +0700

    cập nhật đường dẫn

--- petHealth/my_app/views.py
+++ petHealth/my_app/views.py
@@ -1,35 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 def get_home(request):
     return render(request, 'home.html')  # tra ve trang my_app trong trang web
 def getChatWithAI(request):
-    return render(request, 'fontend/chatWithAI.html')
+    return render(request, 'frontend/chatWithAI.html')
 def getFooter(request):
-    return render(request, 'fontend/footer.html')
+    return render(request, 'frontend/footer.html')
 def getHeader(request):
-    return render(request, 'fontend/header.html')
+    return render(request, 'frontend/header.html')
 def getLogin(request):
-    return render(request, 'fontend/login.html')
+    return render(request, 'frontend/login.html')
 def getRegister(request):
-    return render(request, 'fontend/register.html')
+    return render(request, 'frontend/register.html')
 def getForgotPassword(request):
-    return render(request, 'fontend/forgot-password.html')
+    return render(request, 'frontend/forgot-password.html')
 def getPayment(request):
-    return render(request, 'fontend/payment.html')
+    return render(request, 'frontend/payment.html')
 def getPaymentInfor(request):
-    return render(request, 'fontend/delivery-infor.html')
+    return render(request, 'frontend/delivery-infor.html')
 def getCategory(request):
-    return render(request, 'fontend/category.html')
+    return render(request, 'frontend/category.html')
 def getCategoryManage(request):
-    return render(request, 'fontend/admin/category-manage.html')
+    return render(request, 'frontend/admin/category-manage.html')
 def getCustomerManage(request):
-    return render(request, 'fontend/admin/customer-manage.html')
+    return render(request, 'frontend/admin/customer-manage.html')
 def getProductManagement(request):
-    return render(request, 'fontend/admin/product-management.html')
+    return render(request, 'frontend/admin/product-management.html')
 def getProfileAdmin(request):
-    return render(request, 'fontend/admin/profileAdmin.html')
+    return render(request, 'frontend/admin/profileAdmin.html')
 def getDogKibbleView(request):
-    return render(request, 'fontend/DogKibbleView.html')
+    return render(request, 'frontend/DogKibbleView.html')
+def getPersonal(request):
+    return render(request, 'frontend/personal-page.html')
+def getHomePage(request):
+    return render(request, 'frontend/homePage.html')
