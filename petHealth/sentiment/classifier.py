328112c7ba5d2ff62dcd3e43476a968653bbb23a
macchinguyen123 <23130211@st.hcmuaf.edu.vn>
2025-12-17 11:38:00 +0700

    push xoá danh gia phan loai

--- petHealth/sentiment/classifier.py
+++ petHealth/sentiment/classifier.py
@@ -1,14 +1,14 @@
-from .spam_filter import is_spam
-from .ml_phobert import predict_sentiment
-
-def classify_comment(text: str) -> dict:
-    if is_spam(text):
-        return {
-            "is_spam": True,
-            "sentiment": None
-        }
-
-    return {
-        "is_spam": False,
-        "sentiment": predict_sentiment(text)
-    }
+# from .spam_filter import is_spam
+# from .ml_phobert import predict_sentiment
+#
+# def classify_comment(text: str) -> dict:
+#     if is_spam(text):
+#         return {
+#             "is_spam": True,
+#             "sentiment": None
+#         }
+#
+#     return {
+#         "is_spam": False,
+#         "sentiment": predict_sentiment(text)
+#     }
