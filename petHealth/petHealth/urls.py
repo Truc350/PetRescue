2f36dcad5c6736eca805b0531ac529e8e9e78361
macchinguyen123 <23130211@st.hcmuaf.edu.vn>
2025-12-17 11:39:56 +0700

    push xoá danh gia phan loai

--- petHealth/petHealth/urls.py
+++ petHealth/petHealth/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('review.urls')),
     path('', include('my_app.urls')),
-    path("api/", include("sentiment.urls")),
 ]
